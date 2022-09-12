9ccd04b60c6af341f1d11c33e4c7c5bb4e46920a
Alexis Herrera <aheca96@gmail.com>
2022-09-12 14:31:57 -0300

    database updated

--- database/keys.js
+++ database/keys.js
@@ -5,7 +5,7 @@ module.exports = {
 
     database: {
         host: process.env.HOST,
-        user: "root",
+        user: "cba84083_administrador_database",
         password: process.env.PASSWORD,
         database: process.env.DATABASE,
     }
